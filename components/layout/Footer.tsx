4ed3c1f59a09cc2dee741f1f5ed25136f48e374d
Mohammad Kevin <kvn4.200581@gmail.com>
2026-03-02 07:46:18 +0700

    update footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -71,7 +71,7 @@ export default function Footer() {
                         </p>
                         <br />
                         <br />
-                        <p>Powered by <span className="font-medium text-blue-200">LaundryGo</span></p>
+                        <p>Build by <span className="font-medium text-blue-200">Danendra athallah indiarto</span></p>
                     </div>
                     <div className="flex gap-6">
                         <Link href="#" className="hover:underline">
